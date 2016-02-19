3b72e8d24283b6d97b2de3a4c46fb01d34dee04e
George F Rogers <grogers385@gmail.com>
2016-02-19 07:42:14 -0600

    update

--- essocony/__main__.py
+++ essocony/__main__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from .handlers import app
 from os  import environ
+import sys
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
@@ -9,8 +10,9 @@ try:
 except (KeyError, ValueError) as e:
     port = 8080
 
-print("Listening on:", port)
 server = HTTPServer(app)
 server.bind(port)
 server.start(0)
+sys.stdout.write("Listening on: %d\n" % port)
+sys.stdout.flush()
 IOLoop.instance().start()
