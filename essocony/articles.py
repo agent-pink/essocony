4f8ddfee1a279fa3714beab2f2ac3b9d510e7ef3
George F Rogers <grogers385@gmail.com>
2016-03-09 09:00:09 -0600

    update

--- essocony/articles.py
+++ essocony/articles.py
@@ -11,7 +11,7 @@ class Article(object):
                 break
             lines.append(line)
         self.meta = json.loads("".join(lines))
-        self.meta["date"] = datetime.strptime(str(self.meta["date"]), "%Y-%m-%d %H:%M:%S %Z")
+        self.meta["date"] = datetime.strptime(self.meta["date"], "%Y-%m-%d %H:%M:%S")
         self.contents = "".join(f.readlines())
 
 def load(pat):
